2b4ac4de5ed5ad294759ffb04156f2ca461b3b51
Ashbie B. MWEEMBA <ashbie.b.mweemba@gmail.com>
2024-07-12 18:36:35 +0100

    [Challenge - 5:20] (done)  336.Connecting the Motoko Backend to our JS Frontend

--- src/dbank_0_20_1_frontend/src/App.js
+++ src/dbank_0_20_1_frontend/src/App.js
@@ -7,61 +7,91 @@ import logo from './dbank_logo.png';
 
 class App {
   greeting = '';
+  balance = '000';
 
 
   constructor() {
     this.#render();
   }
 
-  #handleSubmit = async (e) => {
-    e.preventDefault();
-    const name = document.getElementById('name').value;
-    this.greeting = await dbank_0_20_1_backend.greet(name);
-    this.#render();
+  #handleCheckBalance = async () => {
+    var currentBalance = await dbank_0_20_1_backend.checkBalance();
+    // [round-off to 2 decimal places]
+    this.balance = currentBalance ? currentBalance.toFixed(2) : 'loading..';    /*NOTE -  -  change state */
+    // ``OR``  this.balance = currentBalance ? Math.round(currentBalance * 100) / 100 : 'loading..';
+
+    this.#render();  /*NOTE - !important --re-render page after changing state */
   };
-  #handleLoad = async (e) => {
+
+  #handleLoad = async () => {
+     await this.#handleCheckBalance(); 
+     /* or << await this.#handleCheckBalance(); >>  --- I think it doesn't matter because there is already
+      an `await` in the #handleCheckBalance function, and the page gets re-rendered once the awaited
+      response is given (it gets re-rendered at the end of the function).
+      
+      This applies to all lines/sections where << this.#handleCheckBalance(); >> is found 
+      
+      But I think not adding ``await`` is dangerous, the code may not do perform as expected
+      e.g. when the are more things to do after the ``this.#handleCheckBalance()`` line.
+      When I call the ``this.#handleCheckBalance()`` function, does the code stack the function
+      and wait for it to finish executing then continue where it left off OR because it's 
+      asynchronous, does it call the funtion and continue with the next line of the current funtion
+      (not ``this.#handleCheckBalance()``, but the function that called it) , and then does it run
+      more than 1 funtion at a time ? 
+      
+      Does ``await`` make the code execution pause and wait for the awaited response or maybe it makes 
+      another process the waits for the response while the previous process continues with what it was
+      doing ? */
+      this.balance = await dbank_0_20_1_backend.compound();
+      this.#render();
+  };
+
+  #handleTransaction = async (e) => {
     e.preventDefault();
-    var currentValue = await dbank_0_20_1_backend.checkBalance();
-    // var currentValue = '000';
-    document.getElementById('value').innerHTML = currentValue ? currentValue : '...loading';
-   //document.getElementById('value').innerHTML = currentValue;
-   this.#render();
+    const button = e.target.querySelector("#submit-btn");
+    button.setAttribute("disabled", true);
+
+    var topUpAmount = document.getElementById('input-amount').value;
+    var withdrawAmount = document.getElementById('withdrawal-amount').value;
+    //  ******    test      ******
+    //withdrawAmount = withdrawAmount ? withdrawAmount : "it's null";
+    //topUpAmount = topUpAmount ? topUpAmount : "it's null";
+    console.log("\n\t topUp Amount = "+ topUpAmount + "\n\t withdraw Amount = "+ withdrawAmount);
+    /**************************/
+
+    if(topUpAmount) { // or if(document.getElementById('input-amount').value.length != 0)
+      await dbank_0_20_1_backend.topUp(parseFloat(topUpAmount));
+      await this.#handleCheckBalance();
+    } 
+    if(withdrawAmount) {
+      await dbank_0_20_1_backend.withdraw(parseFloat(withdrawAmount));
+      await this.#handleCheckBalance();
+    }
+    document.getElementById('input-amount').value = "";
+    document.getElementById('withdrawal-amount').value = "";
+    button.removeAttribute("disabled");
 
   };
 
   #render() {
-/*     let body = html`
-      <main>
-        <img src="${logo}" alt="DFINITY logo" />
-        <br />
-        <br />
-        <form action="#">
-          <label for="name">Enter your name: &nbsp;</label>
-          <input id="name" alt="Name" type="text" />
-          <button type="submit">Click Me!</button>
-        </form>
-        <section id="greeting">${this.greeting}</section>
-      </main>
-    `; */
     let body = html`
       <main>
         <img src="${logo}" alt="DBank logo" width="100" />
-        <h1>Current Balance: $<span id="value">234</span></h1>
+        <h1>Current Balance: $<span id="value">${this.balance}</span></h1>
         <div class="divider"></div>
         <form action="#">
           <h2>Amount to Top Up</h2>
           <input id="input-amount" type="number" step="0.01" min=0 name="topUp" value=""/>
           <h2>Amount to Withdraw</h2>
           <input id="withdrawal-amount" type="number" name="withdraw" step="0.01" min=0 value=""/>
-          <input id="submit-btn" type="submit" value="Finalise Transaction" />
+          <input id="submit-btn" type="submit" value="Finalise Transaction" "/>
         </form>
       </main>
     `; 
     render(body, document.getElementById('root'));
-    /* document
-      .querySelector('form')
-      .addEventListener('submit', this.#handleSubmit); */
-      window.addEventListener("load",this.#handleLoad);
+
+    window.addEventListener("load",this.#handleLoad);
+    document.querySelector('form').addEventListener('submit', this.#handleTransaction);
   }
 }
 
